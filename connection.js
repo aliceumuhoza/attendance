6b0e6e7dad435f38f44c772ff3783c1a3d3d761e
alicemuhoza <aumuhoza72@gmail.com>
2024-05-22 00:10:11 +0300

    First commit

--- connection.js
+++ connection.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 // Create a connection pool to the MySQL database
 const pool = mysql.createPool({
   connectionLimit: 10,
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'school'
+  host: 'bfxpj7wl76ghazzlr2m1-mysql.services.clever-cloud.com',
+  user: 'uvpancnsdjgep8kd',
+  password: '0k2USAy3pYfYmMp8eLQD',
+  database: 'bfxpj7wl76ghazzlr2m1'
 });
 
 module.exports = pool;
